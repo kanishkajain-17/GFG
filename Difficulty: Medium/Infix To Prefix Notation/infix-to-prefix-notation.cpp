96753b799aac8b837c34727b7124ca3e50d77323
kanishkajain-17 <kanishkajn477@gmail.com>
2026-03-28 16:16:14 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Infix To Prefix Notation/infix-to-prefix-notation.cpp
@@ -0,0 +1,60 @@
+class Solution {
+  public:
+    int isPre(char ch){
+        switch(ch){
+            case '^' : return 3;
+            case '*' : 
+            case '/' : return 2;
+            case '+' : 
+            case '-' : return 1;
+            
+        }
+        return 0;
+    }
+    
+    string infixToPrefix(string& s) {
+        
+        // code here
+        reverse(begin(s), end(s));
+        int n = s.length();
+        stack<char>st;
+        string ans = "";
+        for(int i = 0; i < n; i++){
+            if(s[i] == ')')
+                st.push(s[i]);
+                
+            else if(s[i] >= 'a' && s[i] <= 'z' ||
+                s[i] >= 'A' && s[i] <= 'Z' ||
+                s[i] >= '0' && s[i] <= '9')
+                    ans += s[i];
+                    
+            else if(s[i] == '('){
+                while(st.top() != ')'){
+                    char ch = st.top();
+                    ans += ch;
+                    st.pop();
+                }
+                
+                st.pop();
+            }
+            
+            else{
+                while(!st.empty() && st.top() != ')' && 
+                ((s[i] == '^' && isPre(s[i]) <= isPre(st.top()))
+                || s[i] != '^' && isPre(s[i]) < isPre(st.top() ))){
+                    char ch = st.top();
+                    ans += ch;
+                    st.pop();
+                }
+                st.push(s[i]);
+            }
+        }
+        
+        while(!st.empty()){
+            ans += st.top();
+            st.pop();
+        }
+        reverse(begin(ans), end(ans));
+        return ans;
+    }
+};
