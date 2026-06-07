9bcce79d1243309060c409bcb00f716a51d4c38d
kanishkajain-17 <kanishkajn477@gmail.com>
2026-06-07 15:35:35 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Ceil in BST/ceil-in-bst.cpp
+++ Difficulty: Medium/Ceil in BST/ceil-in-bst.cpp
@@ -1,24 +1,34 @@
+/*
+Definition for Node
+class Node {
+  public:
+    int data;
+    Node* left;
+    Node* right;
+
+    Node(int val) {
+        data = val;
+        left = right = nullptr;
+    }
+};
+*/
+
 class Solution {
   public:
     int findCeil(Node* root, int x) {
         // code here
-         if(!root)
-            return 0;
+        if(root == NULL)
+            return -1;
         int cei = -1;
-        while(root){
+        while (root) {
             if(root->data == x)
                 return root->data;
-            else if(root->data > x){
+            else if (root->data > x){
                 cei = root->data;
                 root = root->left;
-                
             }
-            else{
-                
+            else
                 root = root->right;
-                
-                
-            }
         }
         return cei;
     }
