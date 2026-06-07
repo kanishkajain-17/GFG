46350aa7c4d1cf6ba27b0b8af3370217dbaff46d
kanishkajain-17 <kanishkajn477@gmail.com>
2026-06-07 18:43:28 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Predecessor and Successor/predecessor-and-successor.cpp
+++ Difficulty: Medium/Predecessor and Successor/predecessor-and-successor.cpp
@@ -17,36 +17,35 @@ class Solution {
   public:
     vector<Node*> findPreSuc(Node* root, int key) {
         // code here
-        if(!root)
-            return {};
-        Node* flor = NULL;
         Node* cei = NULL;
-        
-        while(root){
-            if(root->data < key){
-                flor = root;
+        Node* flo = NULL;
+        while (root) {
+            if(root->data < key) {
+                cei = root;
                 root = root->right;
             }
-            else if(root->data > key){
-                cei = root;
+            else if(root->data > key) {
+                flo = root;
                 root = root->left;
             }
             else{
-                if(root->left != NULL){
+                if(root->left != NULL) {
                     Node* temp = root->left;
-                    while(temp->right != NULL)
+                    while (temp->right != NULL)
                         temp = temp->right;
-                    flor = temp;
-                }
-                if(root->right != NULL){
-                    Node* temp = root->right;
-                    while(temp->left != NULL)
-                        temp = temp->left;
                     cei = temp;
                 }
+                if(root->right != NULL) {
+                Node* temp = root->right;
+                while (temp->left != NULL)
+                    temp = temp->left;
+                flo = temp;
+                }
                 break;
+                
             }
+                
         }
-        return {flor, cei};
+        return {cei, flo};
     }
 };
