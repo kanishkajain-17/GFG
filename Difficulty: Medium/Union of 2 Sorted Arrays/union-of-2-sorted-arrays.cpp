48a506042b91d1c3349708c71318b4e71aad78c5
kanishkajain-17 <kanishkajn477@gmail.com>
2026-06-21 23:24:54 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Union of 2 Sorted Arrays/union-of-2-sorted-arrays.cpp
+++ Difficulty: Medium/Union of 2 Sorted Arrays/union-of-2-sorted-arrays.cpp
@@ -2,36 +2,33 @@ class Solution {
   public:
     vector<int> findUnion(vector<int> &a, vector<int> &b) {
         // code here
-        vector<int>ans;
         int m = a.size();
         int n = b.size();
-        int i = 0;
-        int j = 0;
-        
-        while(i < m && j < n){
+        int i = 0, j = 0;
+        vector<int> ans;
+        while (i < m && j < n) {
             
-            if(a[i] <= b[j]){
-                if(ans.size() == 0 || ans.back() != a[i])
+            if(a[i] <= b[j]) {
+                if(ans.size() == 0 || a[i] != ans.back())
                     ans.push_back(a[i]);
-                i++;
+                i += 1;
             }
-            else{
-                if(ans.size() == 0 || ans.back() != b[j])
-                    ans.push_back(b[j]);
-                j++;
+            else {
+                if(ans.size() == 0 || b[j] != ans.back())
+                     ans.push_back(b[j]);
+                j += 1;
             }
         }
-        
-        while(i < m){
-            if(ans.size() == 0 || ans.back() != a[i])
-                ans.push_back(a[i]);
-            i++;
+       
+        while (i < m) {
+            if(a[i] != ans.back())
+                 ans.push_back(a[i]);
+            i += 1;
         }
-        
-        while(j < n){
-            if(ans.size() == 0 || ans.back() != b[j])
-                    ans.push_back(b[j]);
-            j++;
+        while (j < n) {
+            if(b[j] != ans.back())
+                ans.push_back(b[j]);
+            j += 1;
         }
         return ans;
     }
