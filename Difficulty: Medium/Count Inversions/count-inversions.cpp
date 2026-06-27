620da817d70483ae66a955cd68273bcbfe79c40c
kanishkajain-17 <kanishkajn477@gmail.com>
2026-06-27 13:55:04 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Count Inversions/count-inversions.cpp
+++ Difficulty: Medium/Count Inversions/count-inversions.cpp
@@ -1,48 +1,52 @@
 class Solution {
   public:
-    int merge(vector<int> &arr, int start, int mid, int end){
+    int merge(vector<int> &arr, int st, int mid, int end) {
+        
         vector<int> temp;
-        int i = start, j = mid + 1;
+        int i = st, j = mid + 1;
         int invCount = 0;
         
-        while(i <= mid && j <= end){
-            if(arr[i] <= arr[j]){
+        while (i <= mid && j <= end) {
+            
+            if(arr[i] <= arr[j]) {
+                
                 temp.push_back(arr[i]);
-                i++;
+                i += 1;
             }
             else{
-                temp.push_back(arr[j]);
-                j++;
-                invCount += (mid - i) + 1;
+                   temp.push_back(arr[j]);
+                   j += 1;
+                   invCount += mid - i + 1;
             }
         }
-        while(i <= mid){
-             temp.push_back(arr[i]);
-                i++;
+        while (i <= mid) {
+            temp.push_back(arr[i]);
+                i += 1;
         }
-        while(j <= end){
-             temp.push_back(arr[j]);
-                j++;
+        while (j <= end) {
+            temp.push_back(arr[j]);
+                   j += 1;
         }
         
-        for(int idx = 0; idx < temp.size(); idx++)
-            arr[idx + start] = temp[idx];
+        for (int i =0; i < temp.size(); i ++) {
             
+            arr[st + i] = temp[i];
+        }
         return invCount;
-        
     }
-    int mergeSort(vector<int> &arr, int start, int end){
-        if(start < end){
+    int mergeSort(vector<int> &arr, int st, int end) {
+        
+        if(st < end) {
             
-        int mid = start + (end - start) / 2;
-        
-        int leftInvCount = mergeSort(arr, start, mid);
-        int rightInvCount = mergeSort(arr, mid + 1, end);
-        
-        int invCount = merge(arr, start, mid, end);
-        
-        return leftInvCount + rightInvCount + invCount;
-        
+            int mid = st + (end - st) / 2;
+            
+            int leftInvCount = mergeSort(arr, st, mid);
+            int rightInvCount = mergeSort(arr, mid + 1, end);
+            
+            int invCount = merge(arr, st, mid, end);
+            
+            return leftInvCount + rightInvCount + invCount;
+            
         }
         return 0;
     }
