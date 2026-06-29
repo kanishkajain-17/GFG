7610fa78e2fd19e77e47844ac2557ed6e1f77029
kanishkajain-17 <kanishkajn477@gmail.com>
2026-06-29 20:22:26 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Number of occurrence/number-of-occurrence.cpp
+++ Difficulty: Easy/Number of occurrence/number-of-occurrence.cpp
@@ -1,41 +1,37 @@
 class Solution {
   public:
-    int countFreq(vector<int>& nums, int x) {
+    int countFreq(vector<int>& nums, int target) {
         // code here
         int n = nums.size();
-        int left = -1;
-        int right = -1;
+        int flor = n, cei = -1;
         int l = 0, r = n - 1;
         
-        //alag alag calculate kar rahe hai
-        //leftMost
-        while(l <= r) {
+        while (l <= r) {
+            
             int mid = l + (r - l) / 2;
-            if(nums[mid] == x) {
-                left = mid;
-                r = mid - 1;
-            }
-            else if(nums[mid] > x)  
-                r = mid - 1;
-            else
+            
+            if(nums[mid] <= target) {
+                cei = mid;
                 l = mid + 1;
+            }
+            else
+                r = mid - 1;
         }
-
-        //rightMost
         l = 0, r = n - 1;
-        while(l <= r) {
+        while (l <= r) {
+            
             int mid = l + (r - l) / 2;
-            if(nums[mid] == x) {
-                right = mid;
-                l = mid + 1;
-            }
-            else if(nums[mid] > x)  
+            
+            if(nums[mid] >= target) {
+                flor = mid;
                 r = mid - 1;
+            }
             else
                 l = mid + 1;
+                
         }
-        if(left == - 1 && right == -1)
+        if(flor == n && cei == -1)
             return 0;
-        return right - left + 1;
+        return cei - flor + 1;
     }
 };
