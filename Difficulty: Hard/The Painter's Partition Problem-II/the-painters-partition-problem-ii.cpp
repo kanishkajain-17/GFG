ecca9878187c464d7ab1a18784fda87f063030ce
kanishkajain-17 <kanishkajn477@gmail.com>
2026-07-02 00:01:56 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Hard/The Painter's Partition Problem-II/the-painters-partition-problem-ii.cpp
+++ Difficulty: Hard/The Painter's Partition Problem-II/the-painters-partition-problem-ii.cpp
@@ -1,36 +1,42 @@
 class Solution {
   public:
-     bool canPartition (vector<int> &arr, int k, int mid) {
-        int painter = 1;
+    bool canPaint(vector<int>& arr, int mid, int k) {
+
+        int n = arr.size();    
+        int timee = 1;
         int sum = 0;
-        for (int &x : arr) {
-            if(sum + x > mid) {
-                painter += 1;
-                sum = x;
+
+        for (int i = 0; i < n; i ++) {
+
+            sum += arr[i];
+            
+            if(sum > mid) {
+                timee += 1;
+                sum = arr[i];
             }
-            else
-                sum += x;
         }
-        return (painter <= k);
-    } 
+        return timee <= k;
+    }
+    
     int minTime(vector<int>& arr, int k) {
         // code here
-        int n = arr.size();
-        if(n < k)
-            return -1;
-        int ans = 0;
+         int n = arr.size();
+        
         int l = *max_element(begin(arr), end(arr));
         int r = accumulate(begin(arr), end(arr), 0);
-
+        int ans = 0;
+        
         while (l <= r) {
+            
             int mid = l + (r - l) / 2;
-            if(canPartition (arr, k, mid)){
+            
+            if(canPaint (arr, mid, k)) {
                 ans = mid;
                 r = mid - 1;
             }
             else
                 l = mid + 1;
-        } 
+        }
         return ans;
     }
 };
