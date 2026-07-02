32a53bed78b470a96598dd9f30c99862d2099df0
kanishkajain-17 <kanishkajn477@gmail.com>
2026-07-02 12:31:34 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Row with Max 1s in Rowwise Sorted/row-with-max-1s-in-rowwise-sorted.cpp
+++ Difficulty: Medium/Row with Max 1s in Rowwise Sorted/row-with-max-1s-in-rowwise-sorted.cpp
@@ -1,21 +1,45 @@
 class Solution {
   public:
+    int findOcc(vector<int> nums, int x) {
+        
+        int n = nums.size();
+        int flo = -1;
+        
+        int l = 0, r = n - 1;
+        
+        while (l <= r) {
+            
+            int mid = l + (r - l) / 2;
+            
+            if(nums[mid] >= x) {
+                flo = mid;
+                r = mid - 1;
+            }
+            else
+               l = mid + 1;
+        }
+        
+        if(flo == -1)
+            return -1;
+            
+        return n - flo;
+    }
     int rowWithMax1s(vector<vector<int>> &arr) {
         // code here
         int n = arr.size();
-        int ans = -1;
-        int maxCount = 0;
+        int len = -1;
+        int maxLen = -1;
+        int idx = -1;
         
-        for (int i = 0; i < n; i += 1) {
-            int count = 0;
-            for (int j = 0; j < n; j += 1) {
-                if(arr[i][j] == 1)
-                    count += 1;
+        for (int i = 0; i < n; i ++) {
+            
+            len = findOcc(arr[i], 1);
+
+            if(len > maxLen) {
+                maxLen = len;
+                idx = i;
             }
-            if(count > maxCount)
-                ans = i;
-            maxCount  = max(maxCount, count);
-        } 
-        return ans;
+        }
+        return idx;
     }
 };
