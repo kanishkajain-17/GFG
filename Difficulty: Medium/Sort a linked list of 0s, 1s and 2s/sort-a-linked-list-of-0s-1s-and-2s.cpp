41dff5e9d734270dcd3d30f090e3424d7f81981a
kanishkajain-17 <kanishkajn477@gmail.com>
2026-07-12 18:20:13 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Sort a linked list of 0s, 1s and 2s/sort-a-linked-list-of-0s-1s-and-2s.cpp
+++ Difficulty: Medium/Sort a linked list of 0s, 1s and 2s/sort-a-linked-list-of-0s-1s-and-2s.cpp
@@ -14,34 +14,39 @@ class Solution {
   public:
     Node* segregate(Node* head) {
         // code here
-        Node* temp = head;
-        int zero_count = 0;
-        int one_count = 0;
-        int two_count = 0;
+        int zero = 0;
+        int one = 0;
+        int two = 0;
+        
+        Node *temp = head;
         
         while (temp != NULL) {
+            
             if(temp->data == 0)
-                zero_count += 1;
-            else if(temp->data == 1) 
-                one_count += 1;
+                zero += 1;
+            else if(temp->data == 1)
+                one += 1;
             else
-                two_count += 1;
+                two += 1;
+                
             temp = temp->next;
         }
         
         temp = head;
-        while (zero_count --) {
+        
+        while (zero--) {
             temp->data = 0;
             temp = temp->next;
-        } 
-        while (one_count --) {
+        }
+        while (one--) {
             temp->data = 1;
             temp = temp->next;
-        } 
-        while (two_count --) {
+        }
+        while (two--) {
             temp->data = 2;
             temp = temp->next;
-        } 
+        }
+        
         return head;
     }
 };
