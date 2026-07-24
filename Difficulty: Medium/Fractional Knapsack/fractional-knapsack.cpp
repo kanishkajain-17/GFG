275cbf69657f878bd9f35073d912ba099d11c60d
kanishkajain-17 <kanishkajn477@gmail.com>
2026-07-24 09:53:33 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Fractional Knapsack/fractional-knapsack.cpp
+++ Difficulty: Medium/Fractional Knapsack/fractional-knapsack.cpp
@@ -2,34 +2,38 @@ class Solution {
   public:
     double fractionalKnapsack(vector<int>& val, vector<int>& wt, int capacity) {
         // code here
-        int n = wt.size();
+        int n = val.size();
+        double maxVal = 0;
+        
         vector<pair<int, int>> nums;
         
-        for (int i = 0; i < n; i += 1) {
+        for (int i = 0; i < n; i++) {
             nums.push_back({val[i], wt[i]});
         }
         
-        auto lambda = [](pair<int, int> nums1, pair<int, int> nums2) {
-            return ((double)nums1.first / nums1.second) > ((double)nums2.first / nums2.second);
+        auto lambda = [&] (pair<int, int> a, pair<int, int> b) {
+            
+            return ((double)a.first / a.second) > ((double)b.first / b.second); 
         };
-        sort(begin(nums), end(nums), lambda);
-        double cost = 0;
         
-        for (int i = 0; i < n; i += 1) {
+        sort(begin(nums), end(nums), lambda);
+        
+        for (int i = 0; i < n; i++) {
             
             int value = nums[i].first;
-            int weight = nums[i].second;
+            int wtt = nums[i].second;
             
-            if(weight <= capacity) {
-                capacity -= weight;
-                cost += value;
+            if(wtt <= capacity) {
+                
+                maxVal += value;
+                capacity -= wtt;
             }
             else {
-                cost += ((double)capacity / weight) * value;
-                break;
                 
+                maxVal += ((double)capacity / wtt) * value;
+                break;
             }
         }
-        return cost;
+        return maxVal;
     }
 };
