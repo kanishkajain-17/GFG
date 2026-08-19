fc9976bd68351b669341e78339e5db315bd1fa24
kanishkajain-17 <kanishkajn477@gmail.com>
2026-08-19 14:47:50 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Root to Leaf Paths/root-to-leaf-paths.cpp
+++ Difficulty: Medium/Root to Leaf Paths/root-to-leaf-paths.cpp
@@ -1,38 +1,35 @@
-/*
-
-Definition for Binary Tree Node
-struct Node
-{
+/* Definition for Node
+class Node {
+  public:
     int data;
-    struct Node* left;
-    struct Node* right;
-
-    Node(int x){
-        data = x;
-        left = right = NULL;
+    Node* left;
+    Node* right;
+    Node(int val) {
+        data = val;
+        left = right = nullptr;
     }
-};
-*/
+}; */
 
 class Solution {
   public:
-    vector<vector<int>> ans;
-    vector<int>temp;
-    void solve(Node* root){
+    vector<vector<int>>ans;
+    vector<int> temp;
+    void solve(Node* root) {
+        
         if(!root)
             return;
         temp.push_back(root->data);
+        
         if(root->left == NULL && root->right == NULL)
             ans.push_back(temp);
+        
         else{
             solve(root->left);
             solve(root->right);
         }
-        
         temp.pop_back();
-        
     }
-    vector<vector<int>> Paths(Node* root) {
+    vector<vector<int>> paths(Node* root) {
         // code here
         if(!root)
             return {};
