617f1dab0aec474f734a259ed57ccfb85d111171
kanishkajain-17 <kanishkajn477@gmail.com>
2026-08-21 15:44:19 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Hard/Burning Tree/burning-tree.cpp
+++ Difficulty: Hard/Burning Tree/burning-tree.cpp
@@ -1,4 +1,4 @@
-/*
+/* Structure of binary tree Node
 class Node {
   public:
     int data;
@@ -7,83 +7,63 @@ class Node {
 
     Node(int val) {
         data = val;
-        left = right = NULL;
+        left = right = nullptr;
     }
-};
-*/
+};*/
 
 class Solution {
   public:
-  
-    int findMinTime(map<Node*, Node*>& parent, Node* targetAdd){
-        queue<Node*>q;
-        q.push(targetAdd);
-        map<Node*, int>vis;
-        vis[targetAdd] = 1;
-        int time = 0;
-        while(!q.empty()){
-            int n = q.size();
-            int fl = 0; //fl = flag
-            for(int i = 0; i < n; i++){
-                auto node = q.front();
-                q.pop();
-                
-                
-                //checking for left
-                if(node->left && !vis[node->left]){
-                    fl = 1;
-                    vis[node->left] = 1;
-                    q.push(node->left);
-                }
-                //checking for right
-                if(node->right && !vis[node->right]){
-                    fl = 1;
-                    vis[node->right] = 1;
-                    q.push(node->right);
-                }
-                //checking for parent
-                if(parent[node] && !vis[parent[node]]){
-                    fl = 1;
-                    vis[parent[node]] = 1;
-                    q.push(parent[node]);
-                }
-            }
-            if(fl == 1)//mtlb kahi(left, right, parent) toh burn hua hai
-                //bhale kahi ek jagah bhi hua ho lekin uss time mai burn hua hai
-                //toh time badha dennge
-                time++;
-                
-        }
-        return time;
-    }
-  
-    Node* addParentandAddress(Node* root, map<Node*, Node*>& parent, 
-    int target){
-        queue<Node*>q;
-        q.push(root);
-        Node* res;
-        while(!q.empty()){
-            Node* node = q.front();
-            if(node->data == target)//target value ka address mil gaya 
-                res = node;//res mai usska address store karlo
-            q.pop();
-            if(node->left){
-                parent[node->left] = node;
-                q.push(node->left);
-            }
-            if(node->right){
-                parent[node->right] = node;
-                q.push(node->right);
-            }
-        }
-        return res;
+    void makeGraph(unordered_map<int, vector<int>> &adj, int parent, Node* cur) {
+        
+        if(cur == NULL)
+            return;
+        
+        if(parent != -1) 
+            adj[cur->data].push_back(parent);
+            
+        if(cur->left != NULL)
+            adj[cur->data].push_back(cur->left->data);
+            
+        if(cur->right != NULL)
+            adj[cur->data].push_back(cur->right->data);
+            
+        makeGraph(adj, cur->data, cur->left);
+        makeGraph(adj, cur->data, cur->right);
+        
     }
     int minTime(Node* root, int target) {
         // code here
-        map<Node*, Node*>parent;
-        Node* targetAdd = addParentandAddress(root, parent, target);
-        int time = findMinTime(parent, targetAdd);
-        return time;
+        unordered_map<int, vector<int>> adj;
+        makeGraph(adj, -1, root);
         
+        unordered_set<int> visited;
+        visited.insert(target);
+        
+        queue<int> que;
+        que.push(target);
+        
+        int seconds = 0;
+        
+        while (!que.empty()) {
+            
+            int n = que.size();
+            
+            while (n--) {
+                
+                int cur = que.front();
+                que.pop();
+                
+                for (int &ngbr : adj[cur]) {
+                    
+                    if(visited.count(ngbr) == 0) {
+                        visited.insert(ngbr);
+                        que.push(ngbr);
+                    }
+                }
+            } 
+            seconds += 1;
+        }
+        
+        return seconds - 1;
     }
 };
