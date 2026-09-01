5c5d5bb9506266f655f3875e7bd253592f18eb29
kanishkajain-17 <kanishkajn477@gmail.com>
2026-09-01 12:21:11 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Minimum Spanning Tree/minimum-spanning-tree.cpp
+++ Difficulty: Medium/Minimum Spanning Tree/minimum-spanning-tree.cpp
@@ -1,44 +1,46 @@
 class Solution {
   public:
-    typedef pair<int, int>P;
+    typedef pair<int, int> p;
     int spanningTree(int V, vector<vector<int>>& edges) {
         // code here
-        
-        vector<vector<P>>adj(V);
-        for(auto & e : edges){
-            int u = e[0];
-            int v = e[1];
-            int wt = e[2];
-            adj[u].push_back({v, wt});
-            adj[v].push_back({u, wt});
-        }
-        
-        priority_queue<P, vector<P>, greater<P>>pq;
-        pq.push({0, 0}); //{wt, node}
-        vector<int>inMST(V, false);
-        int sum = 0;
-        
-        while(!pq.empty()){
-            auto p = pq.top();
-            pq.pop();
-            
-            int wt = p.first;
-            int node = p.second;
-            
-            if(inMST[node] == true)
+        vector<vector<p>> adj(V);
+         for (auto &e : edges) {
+             int u = e[0];
+             int v = e[1];
+             int wt = e[2];
+             
+             adj[u].push_back({v, wt});
+             adj[v].push_back({u, wt});
+         }
+         
+         vector<int> visited(V, false);
+         int sum = 0;
+         
+         priority_queue<p, vector<p>, greater<p>> pq;
+         pq.push({0, 0});
+         
+         while (!pq.empty()) {
+             
+             int wt = pq.top().first;
+             int node = pq.top().second;
+             pq.pop();
+             
+             if(visited[node] == true)
                 continue;
-            inMST[node] = true;
-            sum += wt;
             
-            for(auto & vec : adj[node]){ // uss node se aage dekho kon
-                //kon connected hai phir udher explore karo
-                int neighbour = vec.first;
-                int neighbour_wt = vec.second;
-                
-                if(inMST[neighbour] == false)
-                    pq.push({neighbour_wt, neighbour});
-            }
-        }
-        return sum;
+             visited[node] = true;
+             sum += wt;
+             
+             for (auto &a : adj[node]) {
+                 
+                 int neighbour_node = a.first;
+                 int neighbour_wt = a.second;
+                 
+                 if(visited[neighbour_node] == false) {
+                     pq.push({neighbour_wt, neighbour_node});
+                 }
+             }
+         }
+         return sum;
     }
 };
